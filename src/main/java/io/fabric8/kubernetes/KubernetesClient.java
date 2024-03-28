7aebd6cc791d4ccf82a51942610cf7ac8ba5ff9c
everest <chayanpathak2017@gmail.com>
2024-03-28 23:24:02 +0530

    Kubecleint added packages commit 1

---
+++ src/main/java/io/fabric8/kubernetes/KubernetesClient.java
@@ -0,0 +1,13 @@
+package io.fabric8.kubernetes;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.api.model.PodList;
+import org.jboss.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import javax.net.ssl.*;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
